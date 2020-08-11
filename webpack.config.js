d88c8d1188ed1d33b2d7abfed71c1cbd9f5ebed3
Deepesh kumar <deepeshk1204>
2020-08-11 17:18:43 +0530

    Added build scripts

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require("path")
 const HtmlWebpackPlugin = require("html-webpack-plugin")
 const webpack = require("webpack")
+const autoprefixer = require("autoprefixer")
+const postcssPresetEnv = require("postcss-preset-env")
 
 module.exports = {
   entry: "./src/index",
@@ -24,12 +26,41 @@ module.exports = {
           loader: "babel-loader",
         },
       },
+      {
+        test: /\.svg$/,
+        use: ["@svgr/webpack"],
+      },
+      {
+        test: /\.png$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              mimetype: "image/png",
+            },
+          },
+        ],
+      },
       // css-loader to bundle all the css files into one file and style-loader to add all the styles  inside the style tag of the document
       {
         test: /\.css$/,
         use: [
           "style-loader",
           { loader: "css-loader", options: { importLoaders: 1 } },
+          {
+            loader: "postcss-loader",
+            options: {
+              ident: "postcss",
+              // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+              plugins: () => [
+                postcssPresetEnv({
+                  autoprefixer: { grid: true },
+                  stage: 2,
+                }),
+                require("tailwindcss"),
+              ],
+            },
+          },
         ],
       },
     ],
